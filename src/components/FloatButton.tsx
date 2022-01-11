2b25dacfcae771144ecbad35a0e7a6b48f4bd4b9
Jorge Ossa <jorgehossa@gmail.com>
2022-01-11 17:51:34 -0500

    Add: Floating button component

--- src/components/FloatButton.tsx
+++ src/components/FloatButton.tsx
@@ -1,37 +1,64 @@
 import React from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  TouchableNativeFeedback,
+  Platform,
+} from 'react-native';
 
 interface FloatButtonProps {
   title: string;
+  position?: 'BL' | 'BR';
   action: () => void;
-  position: 'BL' | 'BR';
 }
 
 const FloatButton = ({ title, action, position }: FloatButtonProps) => {
-  return (
+  const ios = () => (
     <TouchableOpacity
       style={[
+        styles.buttonLocation,
         position === 'BL' ? styles.buttonLocationBL : styles.buttonLocationBR,
       ]}
+      activeOpacity={0.7}
       onPress={action}>
       <View style={styles.button}>
         <Text style={styles.buttonTitle}>{title}</Text>
       </View>
     </TouchableOpacity>
   );
+
+  const android = () => (
+    <View
+      style={[
+        styles.buttonLocation,
+        position === 'BL' ? styles.buttonLocationBL : styles.buttonLocationBR,
+      ]}>
+      <TouchableNativeFeedback
+        onPress={action}
+        background={TouchableNativeFeedback.Ripple('#28425B', false, 30)}>
+        <View style={styles.button}>
+          <Text style={styles.buttonTitle}>{title}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
+  );
+
+  return Platform.OS === 'ios' ? ios() : android();
 };
 
 export default FloatButton;
 
 const styles = StyleSheet.create({
-  buttonLocationBL: {
+  buttonLocation: {
     position: 'absolute',
     bottom: 20,
+  },
+  buttonLocationBL: {
     left: 20,
   },
   buttonLocationBR: {
-    position: 'absolute',
-    bottom: 20,
     right: 20,
   },
   button: {
@@ -41,6 +68,15 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     borderRadius: 100,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+
+    elevation: 7,
   },
   buttonTitle: {
     fontSize: 20,
