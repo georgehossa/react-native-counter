05eeb7ba3513913bab76db56d898ccbcd8421cd6
Jorge Ossa <jorgehossa@gmail.com>
2022-01-11 17:53:11 -0500

    👌 IMPROVE: Remove comments

--- App.tsx
+++ App.tsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import { View, Text } from 'react-native';
-
-// import HelloWorldScreen from './src/screens/HelloWorldScreen';
 import CounterScreen from './src/screens/CounterScreen';
 
 const App = () => {
